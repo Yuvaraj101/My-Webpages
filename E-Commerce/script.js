8e4a3acc6379d2a33321b7b9e7156260cd5c70ad
Yuvaraj101 <yuvarajsk567@gmail.com>
2023-10-26 21:49:49 +0530

    .

--- E-Commerce/script.js
+++ E-Commerce/script.js
@@ -1,1 +1,29 @@
-console.log("dsfs");
+
+var sliderLeftBtn = document.getElementById("slider-left-activate")
+var sliderRightBtn = document.getElementById("slider-right-activate")
+
+// slider container 
+var slider = document.querySelector(".slider-image-container")
+
+var margin = 0
+
+sliderRightBtn.addEventListener("click",function()
+{
+    margin = margin + 100
+    if (margin>200)
+    {
+        margin = 0 
+    }
+    slider.style.marginLeft = "-" + margin + "vw"
+
+
+})
+
+sliderLeftBtn.addEventListener("click",function()
+{
+    margin = margin - 100
+    if (margin < 0){
+        margin = 200
+    }
+    slider.style.marginLeft = "-" + margin + "vw"
+})
